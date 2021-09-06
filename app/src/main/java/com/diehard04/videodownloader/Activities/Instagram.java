8973f3e72d760cac0100d59a0d176cebed8244ba
Dipak Kumar Mehta <dipakm@invendis.com>
2021-09-06 21:14:22 +0530

    first committ

---
+++ app/src/main/java/com/diehard04/videodownloader/Activities/Instagram.java
@@ -0,0 +1,34 @@
+package com.diehard04.videodownloader.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.diehard04.videodownloader.Models.InstagramVideoDownloader;
+import com.diehard04.videodownloader.R;
+
+public class Instagram extends AppCompatActivity {
+
+    EditText inputURl;
+    Button BtnDownload;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instagram);
+
+        inputURl = findViewById(R.id.input_InstaURL);
+        BtnDownload = findViewById(R.id.btn_downloadInstaVideo);
+
+        BtnDownload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String URL = inputURl.getText().toString();
+                InstagramVideoDownloader downloader = new InstagramVideoDownloader(Instagram.this, URL);
+                downloader.DownloadVideo();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/diehard04/videodownloader/Activities/Tiktok.java
@@ -0,0 +1,35 @@
+package com.diehard04.videodownloader.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.diehard04.videodownloader.Models.TiktokVideoDownloader;
+import com.diehard04.videodownloader.R;
+
+public class Tiktok extends AppCompatActivity {
+
+    EditText inputURl;
+    Button BtnDownload;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tiktok);
+
+        inputURl = findViewById(R.id.input_TiktokURL);
+        BtnDownload = findViewById(R.id.btn_downloadTiktokVideo);
+
+        BtnDownload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String URL = inputURl.getText().toString();
+                TiktokVideoDownloader downloader = new TiktokVideoDownloader(Tiktok.this, URL);
+                downloader.DownloadVideo();
+            }
+        });
+    }
+}
